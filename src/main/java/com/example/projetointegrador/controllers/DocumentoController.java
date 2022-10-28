c24d0c8a0d4c27063711fba87ab6e439f74a0b2a
DANILO-DOS-SANTOS-RODRIGUES <dev.danilo.rodrigues@gmail.com>
2022-10-28 19:02:35 -0300

    projeto integrador

--- src/main/java/com/example/projetointegrador/controllers/DocumentoController.java
+++ src/main/java/com/example/projetointegrador/controllers/DocumentoController.java
@@ -1,4 +1,16 @@
 package com.example.projetointegrador.controllers;
 
+import com.example.projetointegrador.dto.DocumentoDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api")
+@CrossOrigin
 public class DocumentoController {
+    @PostMapping(value ="/salvarDocumento")
+    public ResponseEntity<Object>salvarDocumento(@RequestBody DocumentoDTO documentoDTO){
+        System.out.println(documentoDTO);
+        return null;
+    }
 }
