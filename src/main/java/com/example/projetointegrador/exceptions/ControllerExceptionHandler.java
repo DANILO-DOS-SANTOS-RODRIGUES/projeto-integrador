0f9d0a6ed002bfe020cdd4def96249d65721271b
Danilo <dev.danilo.rodrigues@gmail.com>
2022-11-17 19:13:32 -0300

    Colocando a data no padrão brasileiro na tratativa da exception

--- src/main/java/com/example/projetointegrador/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/example/projetointegrador/exceptions/ControllerExceptionHandler.java
@@ -4,9 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import javax.servlet.http.HttpServletRequest;
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @ControllerAdvice
@@ -14,7 +13,7 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<StandardException> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
         StandardException err = new StandardException();
-        err.setTimestamp(String.valueOf(Instant.now()));
+        err.setTimestamp(String.valueOf(LocalDateTime.now()));
         err.setStatus(String.valueOf(HttpStatus.NOT_FOUND.value()));
         err.setError(e.getMessage());
         err.setTrace("nome_arquivo: " + Arrays.stream(
