beaf3387b596506141a0e7059a6242d6f7ad4d3c
DANILO-DOS-SANTOS-RODRIGUES <dev.danilo.rodrigues@gmail.com>
2022-11-18 19:49:29 -0300

    Adicionando o Swagger

--- src/main/java/com/example/projetointegrador/repositories/PessoaRepository.java
+++ src/main/java/com/example/projetointegrador/repositories/PessoaRepository.java
@@ -1,11 +1,11 @@
 package com.example.projetointegrador.repositories;
 
 import com.example.projetointegrador.models.Pessoa;
-import com.example.projetointegrador.models.Taxa;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+ 
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-    List<Pessoa> findByTaxaPorcentagemAndCarteira_SaldoAtual(Double porcentagem, Double saldoAtual);
+
 }
